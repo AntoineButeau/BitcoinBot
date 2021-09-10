363a56e84bd364a2091936fe327b730c7624c56e
Antoine Buteau <abuteau10@gmail.com>
2021-09-10 23:54:38 +0000

    Started building coinmarket BTC price fetching

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import os
+import requests
+import time
 
 from telegram import Update #upm package(python-telegram-bot)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext  #upm package(python-telegram-bot)
 
+#global variables
+cmc_api_key = os.getenv("CMC_TOKEN")
+bot_token = os.getenv("TOKEN")
+threshold = 30000 #Bitcoin price threshold - TODO, need to take into account currency and latest price
+time_interval = 5 * 60 # in seconds, 5 minutes time interval
 
 def start_command(update: Update, context: CallbackContext) -> None:
   pass
 
-def main():
-    updater = Updater(os.getenv("TOKEN"))
+def get_btc_price():
+    pass
 
+def main():
+    # Create a Bot object
+    updater = Updater(bot_token)
     dispatcher = updater.dispatcher
+
+    # Bot start handler
     dispatcher.add_handler(CommandHandler("start", start_command))
 
+    # Start the Bot start_polling() method
+    # Keep the Bot listening using idle() method
     updater.start_polling()
-
     updater.idle()
 
 if __name__ == '__main__':
