20ad2735a20b29a5965c453de4e6a13684c21137
Antoine Buteau <abuteau10@gmail.com>
2021-09-11 00:13:36 +0000

    Send message fn

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 cmc_api_key = os.getenv("CMC_TOKEN")
 bot_token = os.getenv("BOT_TOKEN")
 chat_id = os.getenv ("CHAT_TOKEN")
-threshold = 30000 #Bitcoin price threshold - TODO, need to take into account currency and latest price
+threshold = 30000 #Bitcoin price threshold - TODO, need to take into account currency and latest price, probably also doesnt fit the use case I'm trying to achieve, so need to take into account historical price somewhat
 time_interval = 5 * 60 # in seconds, 5 minutes time interval
 
+# Function when user starts the bot
 def start_command(update: Update, context: CallbackContext) -> None:
   pass
 
+# Function to get BTC price
 def get_btc_price():
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
     headers = {
@@ -22,13 +24,20 @@ def get_btc_price():
         'X-CMC_PRO_API_KEY': cmc_api_key
     }
     
-    # make a request to the coinmarketcap api
+    # Make a request to the coinmarketcap api
     response = requests.get(url, headers=headers)
     response_json = response.json()
 
-    # extract the bitcoin price from the json data
+    # Extract the bitcoin price from the json data - TODO consider user currency when starting
     btc_price = response_json['data'][0]
     return btc_price['quote']['USD']['price']
+
+# Function to send_message through telegram
+def send_message(chat_id, msg):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={msg}"
+
+    # send the msg
+    requests.get(url)
 
 def main():
     # Create a Bot object
