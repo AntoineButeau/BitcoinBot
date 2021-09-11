fa18b4f1d4284e3d9923361ce103408b6a5a7d00
Antoine Buteau <abuteau10@gmail.com>
2021-09-11 14:57:19 +0000

    added config module

---
+++ bitcoinbot/config.py
@@ -0,0 +1,23 @@
+import os
+
+# CoinMarketCap Pro API key
+CMC_API_KEY = os.getenv("CMC_TOKEN")
+
+# The token of the @Replit_Bitcoin_Bot
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# The chat ID to send message
+CHAT_ID = os.getenv("CHAT_TOKEN")
+
+# Do APIs requests with pause
+TIME_INTERVAL = 30 * 60  # in seconds, 30 minutes time interval
+
+# Market symbol to pull from CoinMarketCap
+MARKET_SYMBOL = 'BTC'
+
+# Currency ISO to pull from CoinMarketCap
+CURRENCY_ISO = 'USD'
+
+# Thresholds to notify user
+LOWER_THRESHOLD = 45500  #Bitcoin lower price threshold - TODO, need to take into account currency and latest price, probably also doesnt fit the use case I'm trying to achieve, so need to take into account historical price somewhat
+UPPER_THRESHOLD = 46000  #Bitcoin upper threshold
--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-import os
 import requests
 import time
+import bitcoinbot.config as config
 
 from telegram import Update  #upm package(python-telegram-bot)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext  #upm package(python-telegram-bot)
-
-#global variables
-cmc_api_key = os.getenv("CMC_TOKEN")
-bot_token = os.getenv("BOT_TOKEN")
-chat_id = os.getenv("CHAT_TOKEN")
-market_symbol = 'BTC'
-currency_iso = 'USD'
-lower_threshold = 45500  #Bitcoin lower price threshold - TODO, need to take into account currency and latest price, probably also doesnt fit the use case I'm trying to achieve, so need to take into account historical price somewhat
-upper_threshold = 46000  #Bitcoin upper threshold
-time_interval = 30 * 60  # in seconds, 30 minutes time interval
-
 
 #Function when user starts the bot
 def start_command(update: Update, context: CallbackContext) -> None:
@@ -23,19 +12,19 @@ def start_command(update: Update, context: CallbackContext) -> None:
 #Function to get BTC price
 def get_btc_price():
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-    query = '?symbol=' + market_symbol + '&convert=' + currency_iso
+    query = '?symbol=' + config.MARKET_SYMBOL + '&convert=' + config.CURRENCY_ISO
     headers = {
         'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY': cmc_api_key
+        'X-CMC_PRO_API_KEY': config.CMC_API_KEY
     }
     
     # make a request to the coinmarketcap api
     response = requests.get(url + query, headers=headers)
     response_json = response.json()
     # extract the bitcoin price from the json data
-    btc_price = response_json['data'][market_symbol]
+    btc_price = response_json['data'][config.MARKET_SYMBOL]
 
-    return btc_price['quote'][currency_iso]['price']
+    return btc_price['quote'][config.CURRENCY_ISO]['price']
 
 #Function to handle the price threshold
 def btc_price_threshold():
@@ -43,25 +32,25 @@ def btc_price_threshold():
         price = get_btc_price()
 
         #If the price falls below threshold, send an immediate msg
-        if price < lower_threshold:
-            send_message(chat_id=chat_id, msg=f'BTC Price Drop Alert: {price}')
+        if price < config.LOWER_THRESHOLD:
+            send_message(chat_id=config.CHAT_ID, msg=f'BTC Price Drop Alert: {price}')
 
-        if price > upper_threshold:
-            send_message(chat_id=chat_id, msg=f'BTC Price Rise Alert: {price}')
+        if price > config.UPPER_THRESHOLD:
+            send_message(chat_id=config.CHAT_ID, msg=f'BTC Price Rise Alert: {price}')
 
         #Fetch the price for every time_interval
-        time.sleep(time_interval)
+        time.sleep(config.TIME_INTERVAL)
 
 #Function to send_message through telegram
 def send_message(chat_id, msg):
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={msg}"
+    url = f"https://api.telegram.org/bot{config.BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={msg}"
 
     #Send the msg
     requests.get(url)
 
 def main():
     #Create a Bot object
-    updater = Updater(bot_token)
+    updater = Updater(config.BOT_TOKEN)
     dispatcher = updater.dispatcher
 
     #Bot start handler
