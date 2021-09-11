a757ab844b353830bf2587d6f98a823e9f54a779
Antoine Buteau <abuteau10@gmail.com>
2021-09-11 00:04:59 +0000

    Added Chat ID token

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 
 #global variables
 cmc_api_key = os.getenv("CMC_TOKEN")
-bot_token = os.getenv("TOKEN")
+bot_token = os.getenv("BOT_TOKEN")
+chat_id = os.getenv ("CHAT_TOKEN")
 threshold = 30000 #Bitcoin price threshold - TODO, need to take into account currency and latest price
 time_interval = 5 * 60 # in seconds, 5 minutes time interval
 
@@ -15,7 +16,19 @@ def start_command(update: Update, context: CallbackContext) -> None:
   pass
 
 def get_btc_price():
-    pass
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': cmc_api_key
+    }
+    
+    # make a request to the coinmarketcap api
+    response = requests.get(url, headers=headers)
+    response_json = response.json()
+
+    # extract the bitcoin price from the json data
+    btc_price = response_json['data'][0]
+    return btc_price['quote']['USD']['price']
 
 def main():
     # Create a Bot object
